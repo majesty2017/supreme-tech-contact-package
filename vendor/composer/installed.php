7e8fbf52049f8b2c76379ac776924e674b212e0c
majesty2017 <reborndzre@gmail.com>
2022-12-22 21:00:36 +0000

    Package name changed

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '334f7d1fbcebdc091588821c3349147481b3a35d',
+        'name' => 'supremetech/contact',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'supremetech/contact' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '334f7d1fbcebdc091588821c3349147481b3a35d',
+            'dev_requirement' => false,
+        ),
+    ),
+);
