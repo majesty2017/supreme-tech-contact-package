e272d64799e577634164affcc6ba77423b7d8877
majesty2017 <reborndzre@gmail.com>
2022-12-22 23:38:16 +0000

    Published views

--- src/ContactServiceProvider.php
+++ src/ContactServiceProvider.php
@@ -12,7 +12,9 @@ class ContactServiceProvider extends ServiceProvider
         $this->loadViewsFrom(__DIR__.'/views', 'contact');
         $this->loadMigrationsFrom(__DIR__.'/database/migrations');
         $this->mergeConfigFrom(__DIR__.'/config/contact.php', 'contact');
+
         $this->publishes([
+            __DIR__.'/views' => resource_path('views/vendor/contact'),
             __DIR__.'/config/contact.php' => config_path('contact.php')
         ]);
     }
